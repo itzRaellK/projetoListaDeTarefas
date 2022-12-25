dc9a183dd4f8a61b0ead2c6a676de1b2b88f19ec
itzRaell <israel-kessler@hotmail.com>
2022-12-25 19:54:19 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("form").on("submit", function(e) {
+        e.preventDefault();
+
+        const novaTarefa = $("#nova-tarefa").val();
+        const novoItem = $('<li style="display: none"></li>');
+
+        $(`
+            <li>
+                ${novaTarefa}
+            </li>
+        `).appendTo(novoItem);
+
+        $(novoItem).appendTo("ul");
+        $(novoItem).fadeIn();
+        $("#nova-tarefa").val("");
+
+        $("li").click(function() {
+            $(this).parents("li").css({"text-decoration":"line-through"});
+        })
+    })
+})
